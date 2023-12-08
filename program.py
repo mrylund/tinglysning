ef462b78270238823b92da59ad7d864c1c2439c0
Martin Rylund <martin@rylund.eu>
2023-12-08 11:38:35 +0100

    Add logic to get UUID and summary from hovednoteringsnummer

--- program.py
+++ program.py
@@ -1,5 +1,7 @@
+import sys
 import yaml
 from tinglysning_cookies import get_session_cookies
+from tinglysning_rest import get_property, get_property_summary
 
 def main():
   # Load config
@@ -8,6 +10,16 @@ def main():
 
   # Get cookies
   cookies = get_session_cookies(config)
+  
+  hovednoteringsnummer = sys.argv[1] # This is for testing purpose and will be removed later
+  property = get_property(config, cookies, hovednoteringsnummer)
+  if property is None: sys.exit(0)
+  
+  summary = get_property_summary(config, cookies, property["items"][0]["uuid"])
+  if summary is None: sys.exit(0)
+  
+  print(summary["EjendomSummariskHentResultat"]["ns:EjendomSummarisk"]["ns7:ModelId"])
+  
 
 
 if __name__ == "__main__":
