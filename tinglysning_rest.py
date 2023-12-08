daff4acf20da21574e2b49de5430885a366f4a99
Martin Rylund <martin@rylund.eu>
2023-12-08 11:26:48 +0100

    Add function for GET requests to tinglysning

--- tinglysning_rest.py
+++ tinglysning_rest.py
@@ -1,3 +1,6 @@
+import requests
+import xmltodict
+
 def __check_status_code(res):
   print(res.status_code)
   if res.status_code in range (200, 300):
@@ -7,3 +10,21 @@ def __check_status_code(res):
   elif res.status_code in range (500, 600):
     return False, False # Success, continue
 
+def __get_request_url(url, cookies, config):
+  headers = {
+    "Cookie": f"TDK_JSESSIONID={cookies['TDK_JSESSIONID']}; TDK_CSRFTOKEN={cookies['TDK_CSRFTOKEN']}",
+    "Accept": "application/json",
+  }
+  res = requests.get(url, headers=headers)
+
+  success, continue_ = __check_status_code(res)
+  if not success:
+    if not continue_:
+      exit(config["error_messages"]["error_5xx"])
+    else:
+      return None
+
+  if res.headers['content-type'].find("application/json") != -1:
+    return res.json()
+  elif res.headers['content-type'].find("application/xml") != -1:
+    return xmltodict.parse(res.content)
